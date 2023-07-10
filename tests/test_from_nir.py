ec1c4d3a912b9cd20ec159f5ee1efa05e35c825e
Sadique Sheik <sadique.sheik@synsense.ai>
2023-07-10 21:44:59 +0200

    ruff fixes

--- tests/test_from_nir.py
+++ tests/test_from_nir.py
@@ -2,17 +2,17 @@ import torch
 import norse
 
 import nir
-from nirtorch.from_nir import load, GraphExecutor
+from nirtorch.from_nir import load
 
 import pytest
 
 
 def _torch_model_map(m: nir.NIRNode, device: str = "cpu") -> torch.nn.Module:
     if isinstance(m, nir.Linear):
-        l = torch.nn.Linear(*m.weights.shape[-2:])
-        l.weight.data = torch.tensor(m.weights, device=device)
-        l.bias.data = torch.tensor(m.bias, device=device)
-        return l
+        lin = torch.nn.Linear(*m.weights.shape[-2:])
+        lin.weight.data = torch.tensor(m.weights, device=device)
+        lin.bias.data = torch.tensor(m.bias, device=device)
+        return lin
     if isinstance(m, norse.torch.LIFBoxCell):
         return nir.LIF(
             m.p.tau_mem_inv, m.p.v_leak, torch.ones_like(m.p.tau_mem_int), m.p.v_th
@@ -26,7 +26,7 @@ def _torch_model_map(m: nir.NIRNode, device: str = "cpu") -> torch.nn.Module:
 def test_extract_empty():
     g = nir.NIR([], [])
     with pytest.raises(ValueError):
-        m = load(g, _torch_model_map)
+        _ = load(g, _torch_model_map)
 
 
 def test_extract_lin():
