c7aba27bd2b10aa6e4abe5d8f14757451c4f3303
Jens Egholm Pedersen <jens@jepedersen.dk>
2023-10-21 22:53:17 +0200

    Fix tests (#22)
    * Used bleeding edge of sinabs for tests
    
    Closes #21

--- tests/test_from_nir.py
+++ tests/test_from_nir.py
@@ -29,9 +29,9 @@ def test_extract_empty():
 
 
 def test_extract_illegal_name():
-    graph = nir.NIRGraph({"a.b": nir.Input(np.ones(1))}, [])
+    graph = nir.NIRGraph({"a.b": nir.Input(np.ones(1)), "a.c": nir.Linear(np.array([[1.]]))}, [("a.b", "a.c")])
     torch_graph = load(graph, _torch_model_map)
-    assert "a_b" in torch_graph._modules
+    assert "a_c" in torch_graph._modules
 
 
 def test_extract_lin():
