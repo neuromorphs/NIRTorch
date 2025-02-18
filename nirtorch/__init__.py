3114af707261d00d3179f748271dad424898ba27
Jens E. Pedersen <jens.pedersen@innatera.com>
2025-02-18 13:23:50 +0100

    Fixed version numbering in __init__.py

--- nirtorch/__init__.py
+++ nirtorch/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version as metadata_version, PackageNotFoundError
 
 try:
-    __version__ = version = metadata_version("nir")
+    __version__ = version = metadata_version("nirtorch")
     del metadata_version
 except PackageNotFoundError:
     # package is not installed
@@ -11,9 +11,6 @@ from .from_nir import load  # noqa F401
 from .to_nir import extract_nir_graph  # noqa F401
 from .nir_interpreter import nir_to_torch
 from .torch_tracer import torch_to_nir
-
-__version__ = version = "1.0"
-
 
 __all__ = [
     "extract_torch_graph",
